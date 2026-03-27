2e9c50db3057319841301b575510cf37d2ea3581
Pablo <pablocoronati@gmail.com>
2026-03-26 23:20:23 -0300

    Subo practica_4

---
+++ practica_4/Drivers/API/Inc/API_debounce.h
@@ -0,0 +1,35 @@
+#ifndef API_DEBOUNCE_H
+#define API_DEBOUNCE_H
+
+#include "API_delay.h"
+
+/* Inicializa la MEF */
+void debounceFSM_init(void);
+
+/* Actualiza la MEF (debe llamarse periódicamente) */
+void debounceFSM_update(void);
+
+/**
+ * @brief  Acción ante flanco descendente confirmado (botón presionado).
+ *         Llamada internamente por debounceFSM_update().
+ * @param  Ninguno
+ * @retval Ninguno
+ */
+void buttonPressed(void);
+
+**
+ * @brief  Acción ante flanco ascendente confirmado (botón liberado).
+ *         Llamada internamente por debounceFSM_update().
+ * @param  Ninguno
+ * @retval Ninguno
+ */
+void buttonReleased(void);
+
+/**
+ * @brief Devuelve true si hubo un evento de tecla presionada
+ *        (flanco descendente).
+ *        Resetea automáticamente el estado interno.
+ */
+bool_t readKey(void);
+
+#endif
